e0bd406e3def899451efd64dbe4a79ea4a6b929f
Hardik Sharma <sharmahardikaz@gmail.com>
2023-10-27 00:35:31 +0530

    Body content done

--- myapp/src/components/apiContainer.svelte
+++ myapp/src/components/apiContainer.svelte
@@ -1,9 +1,11 @@
 <script>
   import Input from "./apibox/inputBox.svelte"
   import ContentType from "./apibox/contentType.svelte";
+  import BodyContent from "./apibox/bodyContent.svelte";
 </script>
 
 <div class="APIcontainer">
 <Input />
 <ContentType />
+<BodyContent />
 </div>
