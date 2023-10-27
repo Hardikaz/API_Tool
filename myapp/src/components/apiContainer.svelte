29813e7ef43b8ffcbbb0600b6d6e9ee5ef6d3a84
Hardik Sharma <sharmahardikaz@gmail.com>
2023-10-27 15:38:52 +0530

    API Box completed

--- myapp/src/components/apiContainer.svelte
+++ myapp/src/components/apiContainer.svelte
@@ -2,10 +2,14 @@
   import Input from "./apibox/inputBox.svelte"
   import ContentType from "./apibox/contentType.svelte";
   import BodyContent from "./apibox/bodyContent.svelte";
+  import RespContent from "./apibox/responsePart.svelte";
 </script>
 
+<div class="apiBox">
 <div class="APIcontainer">
 <Input />
 <ContentType />
 <BodyContent />
+<RespContent />
+</div>
 </div>
