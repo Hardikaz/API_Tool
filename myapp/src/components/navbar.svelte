97b924686d15438c66e1c7413874dadc1cd8b4aa
Hardik Sharma <sharmahardikaz@gmail.com>
2023-10-27 15:53:56 +0530

    Navbar ready

--- myapp/src/components/navbar.svelte
+++ myapp/src/components/navbar.svelte
@@ -7,12 +7,13 @@
 <Navbar  class="navbar">
   <NavBrand href="/">
 
-    <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">&lt/Devstar&gtstar </span>
+    <span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white devStr">&lt/Devstar&gtstar </span>
   </NavBrand>
   <NavHamburger  />
   <NavUl >
-    <NavLi href="/">Home</NavLi>
-    <NavLi href="/about">About</NavLi>
-    <NavLi href="/docs/components/navbar">Roadmap</NavLi>
+    <NavLi class="navList" href="/">Home</NavLi>
+    <NavLi class="navList" href="/about">About</NavLi>
+    <NavLi class="navList" href="/about">About</NavLi>
+    <NavLi class="navList" href="/docs/components/navbar">Roadmap</NavLi>
   </NavUl>
 </Navbar>
