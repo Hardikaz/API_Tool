00a80437201f13fce05d3c58705985e6a7bd3a66
Hardik Sharma <sharmahardikaz@gmail.com>
2023-10-29 01:22:57 +0530

    Typo in footer corrected

--- myapp/src/components/footer.svelte
+++ myapp/src/components/footer.svelte
@@ -6,7 +6,7 @@
 <Footer class="footer">
   <FooterCopyright href="/" by="Alhansat Solutions" year={2023} />
   <FooterLinkGroup ulClass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
-    <FooterLink href="/">Terms ans Conditions</FooterLink>
+    <FooterLink href="/">Terms and Conditions</FooterLink>
     <FooterLink href="/">Privacy Policy</FooterLink>
   </FooterLinkGroup>
   
