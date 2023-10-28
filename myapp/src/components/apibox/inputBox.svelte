1b12ac49577c4bc0a3b94e68654848ea90794336
Hardik Sharma <sharmahardikaz@gmail.com>
2023-10-29 02:21:05 +0530

    Method button binded

--- myapp/src/components/apibox/inputBox.svelte
+++ myapp/src/components/apibox/inputBox.svelte
@@ -4,19 +4,24 @@ import { Button } from 'flowbite-svelte';
 import { Dropdown, DropdownItem, DropdownDivider, DropdownHeader } from 'flowbite-svelte';
 import { ChevronDownSolid } from 'flowbite-svelte-icons';
     
+let method="GET";
+
+function handleClick(event){
+  method=event.target.value
+}
 </script>
 
 <div class="inputBox">
 
 
-<Button class="dropDownBtn">GET <ChevronDownSolid class="w-3 h-3 ml-2 text-white dark:text-white" /></Button>
+<Button class="dropDownBtn">{method}<ChevronDownSolid class="w-3 h-3 ml-2 text-white dark:text-white" /></Button>
 
 <Dropdown class="dropDown">
-  <DropdownItem>GET</DropdownItem>
-  <DropdownItem>POST</DropdownItem>
-  <DropdownItem>PUT</DropdownItem>
-  <DropdownItem>DELETE</DropdownItem>
-  <DropdownItem>HEAD</DropdownItem>
+  <DropdownItem on:click={handleClick} value="GET">GET</DropdownItem>
+  <DropdownItem on:click={handleClick} value="POST">POST</DropdownItem>
+  <DropdownItem on:click={handleClick} value="PUT">PUT</DropdownItem>
+  <DropdownItem on:click={handleClick} value="DELETE">DELETE</DropdownItem>
+  <DropdownItem on:click={handleClick} value="HEAD">HEAD</DropdownItem>
 </Dropdown>
 
 <div class="mb-6">
